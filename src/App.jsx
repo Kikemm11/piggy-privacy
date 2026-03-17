9832e9270ddb2b069b1919a190ddabd3fdc53101
Kikemm11 <Kikemaldonado11@gmail.com>
2026-03-17 17:10:43 -0400

    Update base url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PrivacyPolicy from './routes/PrivacyPolicy';
 
 export default function App() {
   return (
-    <Router basename="/privacy"> {/* Use the base path here */}
+    <Router basename="/piggy-privacy"> {/* Use the base path here */}
       <Routes>
         <Route path="/" element={<PrivacyPolicy />} /> {/* Make Home relative to "/privacy" */}
       </Routes>
